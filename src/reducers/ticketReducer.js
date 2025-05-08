3f9c77d69efea6a3375d3da5b5beedcf9f6e1b7d
nikitaax <nikkitanandankar@gmail.com>
2025-05-08 13:42:36 +0530

    formatted

--- src/reducers/ticketReducer.js
+++ src/reducers/ticketReducer.js
@@ -10,29 +10,30 @@ export default function ticketReducer(state, action) {
         ),
         editingTicket: null,
       };
-      case "DELETE_TICKET":
-          if (state.editingTicket && state.editingTicket.id === action.payload.id) {
-            return {
-                ...state,
-                tickets: state.tickets.filter((ticket) => ticket.id !== action.payload.id), 
-                editingTicket: null,
-              };
-          }
+    case "DELETE_TICKET":
+      if (state.editingTicket && state.editingTicket.id === action.payload.id) {
+        return {
+          ...state,
+          tickets: state.tickets.filter(
+            (ticket) => ticket.id !== action.payload.id
+          ),
+          editingTicket: null,
+        };
+      } else {
+        return {
+          ...state,
+          tickets: state.tickets.filter(
+            (ticket) => ticket.id !== action.payload.id
+          ),
+        };
+      }
 
-          else {
-            return {
-                ...state,
-                tickets: state.tickets.filter((ticket) => ticket.id !== action.payload.id), 
-              };
-          }
-     
     case "SET_EDITING_TICKET":
       return { ...state, editingTicket: action.payload };
     case "CLEAR_EDITING_TICKET":
-
-          return { ...state, editingTicket: null };
+      return { ...state, editingTicket: null };
     case "SET_SORTING":
-          return {...state, sortPreference: action.payload };
+      return { ...state, sortPreference: action.payload };
 
     default:
       return state;
