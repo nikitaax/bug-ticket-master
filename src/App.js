73c3e7d838a4e253e974b00faa236e41940ab35b
nikitaax <nikkitanandankar@gmail.com>
2025-05-08 13:40:23 +0530

    sorting feature

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import { useReducer } from 'react';
 import TicketForm from './components/TicketForm';
 import ticketReducer from './reducers/ticketReducer';
 import TicketList from './components/TicketList';
+import { sortTickets } from './utilities/sortingUtility';
 
 function App() {
 
-  const initialState = { tickets: [] , editingTicket: null};
+  const initialState = {
+    tickets: [], editingTicket: null, sortPreference: 'High to Low'
+  };
   
   const [state, dispatch] = useReducer(ticketReducer, initialState);
+
+  const sortedTickets = sortTickets(state.tickets, state.sortPreference);
 
   return (
     <div className="App">
@@ -21,7 +26,12 @@ function App() {
         {state.tickets.length > 0 && (
           <div className='results'>
             <h2> All Tickets</h2>
-            <TicketList tickets={state.tickets} dispatch={dispatch} />
+            <select onChange={e => dispatch({ type: 'SET_SORTING', payload: e.target.value })}>
+              <option value="High to Low"> High to Low</option>
+              <option value="Low to High"> Low to High</option>
+            </select>
+
+            <TicketList tickets={sortedTickets} dispatch={dispatch} />
             </div>
         )}
 
